035c8542bbc09257a818e342051044bbc976df23
Dragon Prevost <dragon@Dragons-MacBook-Pro.local>
2018-09-25 18:58:43 -0700

    layout draft

--- brnkl-payment/src/app/app.component.ts
+++ brnkl-payment/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PaymentComponent } from './payment/payment.component';
+import { Http, Headers} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,44 @@ import { PaymentComponent } from './payment/payment.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http: Http) {}
+
+  //Account variables.
   user_name = 'Moby Dick';
   vessel_type = 'newport-30';
   device_id = '123456789';
   no_sub = false;
-  current_sub = 'monthly'
-  //constructor(private paymentComp: PaymentComponent) { }
+  current_sub = 'monthly';
+  sub_cost = 100.00;
+
+
+  chargeCreditCard() {
+    let form = document.getElementsByTagName("form")[0];
+    (<any>window).Stripe.card.createToken({
+      number: form.cardNumber.value,
+      exp_month: form.expMonth.value,
+      exp_year: form.expYear.value,
+      cvc: form.cvc.value
+    }, (status: number, response: any) => {
+      if (status === 200) {
+        let token = response.id;
+        this.chargeCard(token);
+      } else {
+        console.log(response.error.message);
+      }
+      
+
+    });
+  }
+
+  chargeCard(token: string) {
+    const headers = new Headers({'token': token, 'amount': 100});
+    this.http.post('http://localhost:8080/payment/charge', {}, {headers: headers})
+      .subscribe(resp => {
+        console.log(resp);
+      })
+  }
+
+  
+  
 }
